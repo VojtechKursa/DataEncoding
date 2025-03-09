bcb720fb0dabf82ed5ee1372ed60c3b9fcd066a9
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2025-03-09 19:10:24 +0100

    Add JSON serialization through reflection

--- JSON/JSONBase.cs
+++ JSON/JSONBase.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using DataEncoding.Interfaces;
 
 namespace DataEncoding.JSON
 {
     /// <summary>
     /// Serves as a base for all JSON data objects.
     /// </summary>
-    public abstract class JSONBase
+    public abstract class JSONBase : ISupportsEncode<string>, ISupportsDecode<string>
     {
         /// <summary>
         /// Encodes the data into the JSON format.
@@ -17,9 +18,12 @@ namespace DataEncoding.JSON
         /// Decodes the given JSON data and sets object's properties based on the decoded data.
         /// </summary>
         /// <param name="json">The JSON data to decode.</param>
-        /// <param name="start">The index from which to start decoding.</param>
         /// <returns>The index of the first character after the decoded value.</returns>
         /// <exception cref="ArgumentException"/>
+        public int Decode(string json) => Decode(json, 0);
+
+        /// <inheritdoc cref="Decode(string)"/>
+        /// <param name="start">The index from which to start decoding.</param>
         public abstract int Decode(string json, int start);
 
         /// <summary>
---
+++ Reflection/Serializers/SerializerJSON.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using DataEncoding.JSON;
+using DataEncoding.Reflection.Data;
+
+namespace DataEncoding.Reflection.Serializers
+{
+    public class SerializerJSON : Serializer<string, JSONBase>
+    {
+        internal override JSONBase SerializeInternal(string _, List<Tuple<PropertySerializationData, object>> properties)
+        {
+            JSONNameValuePairCollection collection = new JSONNameValuePairCollection();
+
+            foreach (var property in properties)
+            {
+                collection.Add(property.Item1.Name, Convert(property.Item2));
+            }
+
+            return new JSONObject(collection);
+        }
+
+        internal JSONBase Convert(object value)
+        {
+            if (value == null)
+            {
+                return new JSONNull();
+            }
+            else if (value is bool b)
+            {
+                return new JSONBool(b);
+            }
+            else if (value.IsNumeric())
+            {
+                return new JSONNumber((double)value);
+            }
+            else if (value.IsCollection())
+            {
+                JSONArray array = new JSONArray();
+                foreach (object val in value.ToEnumerable())
+                {
+                    array.Content.Add(Convert(val));
+                }
+                return array;
+            }
+            else if (value.GetType().IsClass)
+            {
+                return SerializeInternal(value);
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
